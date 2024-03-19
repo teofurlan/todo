f3fc4b4451687729b3755209f862da1d13a3c1e4
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-03-19 00:02:46 -0300

    :p

--- src/components/script.js
+++ src/components/script.js
@@ -1,15 +1,13 @@
 
-const checkbox = document.getElementById('checkbox')
-const strikethrough = document.getElementById('strikethrough')
 
-checkbox.addEventListener('change', () => {
-    console.log(checkbox.checked)
-    if (checkbox.checked) {
-        // strikethrough.classList.remove('.bg-black')
-        strikethrough.style.backgroundColor = 'white'
-    }
-    else
-    {
-        strikethrough.style.backgroundColor = 'black'
-    }
-})
+// const clickCheckbox = (index) => {
+//   const checkbox = document.getElementById(`checkbox${index}`);
+//   console.log(index);
+//   const strikethrough = document.getElementById(`strikethrough${index}`);
+//   console.log(checkbox.checked);
+//   if (checkbox.checked) {
+//     strikethrough.style.backgroundColor = "white";
+//   } else {
+//     strikethrough.style.backgroundColor = "black";
+//   }
+// };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			white: '#ffffff',
+			black: '#000000',
+			hint: '#d0d0d0',
+			negativeHint: '#2f2f2f',
+			selected: '#2fafaf',
+			red: '#ff0000'
+		}
 	},
 	plugins: [],
 }
